07baca02d1be1ad041ee4109bd164fffa2c0f9ed
tevaughan <tevaughan@gmail.com>
2016-01-02 16:28:23 -0700

    Added comments.

--- ex-1-1.c
+++ ex-1-1.c
@@ -1,6 +1,9 @@
 
-#include <stdio.h>
+#include <stdio.h>  /* We need this so that we can call printf(). */
 
+/* In C (unlike C++), the return type of a function is not required.  So we
+ * need not write 'int main()'. If the return type be not specified, then by
+ * default it is 'int', but we need not have a 'return' statement. */
 main()
 {
    printf("hello, world!\n");
