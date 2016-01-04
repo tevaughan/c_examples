8d66701f5a05f986bb49705f437bcf26ca51348d
Nicholas Vaughan <nick@HAL2000.Nicks_domain>
2016-01-03 19:12:20 -0700

    finilized comments

--- ex-1-2-nrv.c
+++ ex-1-2-nrv.c
@@ -2,12 +2,14 @@
 
 int main()
 {
-   printf("backslash-a '\a'\n");
-   printf("backslash-b '\b'\n");
-   printf("backslash-c '\c'\n");
+   /* No Comment means that printf returns the character and wastes compiling
+    * time*/
+   printf("backslash-a '\a'\n"); /* control g */
+   printf("backslash-b '\b'\n"); /* goes back 1 space and replaces character */
+   printf("backslash-c '\c'\n"); 
    printf("backslash-d '\d'\n");
-   printf("backslash-e '\e'\n");
-   printf("backslash-f '\f'\n");
+   printf("backslash-e '\e'\n"); /* escape key*/
+   printf("backslash-f '\f'\n"); 
    printf("backslash-g '\g'\n");
    printf("backslash-h '\h'\n");
    printf("backslash-i '\i'\n");
@@ -15,23 +17,94 @@ int main()
    printf("backslash-k '\k'\n");
    printf("backslash-l '\l'\n");
    printf("backslash-m '\m'\n");
-   printf("backslash-n '\n'\n");
+   printf("backslash-n '\n'\n"); /* new line */
    printf("backslash-o '\o'\n");
    printf("backslash-p '\p'\n");
    printf("backslash-q '\q'\n");
-   printf("backslash-r '\r'\n");
+   printf("backslash-r '\r'\n"); /* control m */
    printf("backslash-s '\s'\n");
-   printf("backslash-t '\t'\n");
+   printf("backslash-t '\t'\n"); /* tab */
    /* requires hex characters
    printf("backslash-u '\u'\n");
    */
-   printf("backslash-v '\v'\n");
+   printf("backslash-v '\v'\n"); /* control k */
    printf("backslash-w '\w'\n");
    /* requires hex characters
    printf("backslash-x '\x'\n");
    */
    printf("backslash-y '\y'\n");
    printf("backslash-z '\z'\n");
+   printf("backslash-A '\A'\n");
+   printf("backslash-B '\B'\n");
+   printf("backslash-C '\C'\n");
+   printf("backslash-D '\D'\n");
+   printf("backslash-E '\E'\n"); /* escape key */
+   printf("backslash-F '\F'\n");
+   printf("backslash-G '\G'\n");
+   printf("backslash-H '\H'\n");
+   printf("backslash-I '\I'\n");
+   printf("backslash-J '\J'\n");
+   printf("backslash-K '\K'\n");
+   printf("backslash-L '\L'\n");
+   printf("backslash-M '\M'\n");
+   printf("backslash-N '\N'\n");
+   printf("backslash-O '\O'\n");
+   printf("backslash-P '\P'\n");
+   printf("backslash-Q '\Q'\n");
+   printf("backslash-R '\R'\n");
+   printf("backslash-S '\S'\n");
+   printf("backslash-T '\T'\n");
+   /* error: incomplete universal character name \U
+   printf("backslash-z '\U'\n");
+   */
+   printf("backslash-V '\V'\n");
+   printf("backslash-W '\W'\n");
+   printf("backslash-X '\X'\n");
+   printf("backslash-Y '\Y'\n");
+   printf("backslash-Z '\Z'\n");
+   printf("backslash-1 '\1'\n"); /* control A */
+   printf("backslash-2 '\2'\n"); /* control b */
+   printf("backslash-3 '\3'\n"); /* control c */
+   printf("backslash-4 '\4'\n"); /* control d */
+   printf("backslash-5 '\5'\n"); /* control e */
+   printf("backslash-6 '\6'\n"); /* control f */
+   printf("backslash-7 '\7'\n"); /* control g */
+   printf("backslash-8 '\8'\n");
+   printf("backslash-9 '\9'\n");
+   printf("backslash-0 '\0'\n");/* terminates string before ' */
+   printf("backslash-! '\!'\n");
+   printf("backslash-@ '\@'\n");
+   printf("backslash-# '\#'\n");
+   printf("backslash-$ '\$'\n");
+   printf("backslash-percent '\%'\n");
+   printf("backslash-^ '\^'\n");
+   printf("backslash-& '\&'\n");
+   printf("backslash-* '\*'\n");
+   printf("backslash-( '\('\n");
+   printf("backslash-) '\)'\n");
+   printf("backslash-- '\-'\n");
+   printf("backslash-_ '\_'\n");
+   printf("backslash-= '\='\n");
+   printf("backslash-+ '\+'\n");
+   printf("backslash-` '\`'\n");
+   printf("backslash-~ '\~'\n");
+   printf("backslash-[ '\['\n");
+   printf("backslash-] '\]'\n");
+   printf("backslash-{ '\{'\n");
+   printf("backslash-} '\}'\n");
+   printf("backslash-; '\;'\n");
+   printf("backslash-: '\:'\n");
+   printf("backslash-' '\''\n");
+   printf("backslash-double-quote '\"'\n");
+   printf("backslash-, '\,'\n");
+   printf("backslash-. '\.'\n");
+   printf("backslash-< '\<'\n");
+   printf("backslash-> '\>'\n");
+   printf("backslash-/ '\/'\n");
+   printf("backslash-? '\?'\n");
+   printf("backslash-| '\|'\n");
+   printf("backslash-space '\ '\n");
+   printf("backslash-backslash '\\'\n");
    return 0;
-} 
+}
 
