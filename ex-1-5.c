0c22c858cd264c6932bb1417e0a8bfa92e0a0c0b
Thomas E. Vaughan <tevaughan@nort>
2016-01-10 21:13:19 -0700

    Added ex-1-5 to Makefile. Added ex-1-5 and ex-1-5.out to .gitignore.
    Fixed bugs in ex-1-5.c.
     - Put new line after table's heading.
     - Now decrement (rather than increment) loop variable fahr.
     - Right-justify output columns with column headings.
     - Provide one (rather than zero) decimal digit of precision for Celsius
       column.

--- ex-1-5.c
+++ ex-1-5.c
@@ -3,22 +3,19 @@
 
 int main()
 {
-   /* print Fahrenheit-Celsius table in reverse order*/
+   /* Print Fahrenheit-Celsius table in reverse order. */
 
    int step, lower, upper, fahr;
    float cel;
    
-   step = 20; /* increment */
-   lower = 0; /* lower limit at end */
+   step  =  20; /* increment */
+   lower =   0; /* lower limit at end */
    upper = 300; /* upper limit at beginning */
 
-   printf("Fahrenheit\tCelsius"); /* prints heading */
-
-   for (fahr = upper; fahr >= lower; fahr = fahr + step)
-   {
-      /* prints table */
+   printf("Fahrenheit  Celsius\n"); /* Print heading. */
+   for (fahr = upper; fahr >= lower; fahr = fahr - step) {
       cel = (5.0/9.0) * (fahr - 32);
-      printf("%3d %6.0f\n", fahr, cel);
+      printf("       %3d   %6.1f\n", fahr, cel);
    }
 
    return 0;
